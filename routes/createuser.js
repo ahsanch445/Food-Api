6472f0321d4da536aff8973bbc8a3347706a5e74
Ahsan Ch <ach374392@gmail.com>
2023-12-18 12:35:27 -0800

    add final change

--- routes/createuser.js
+++ routes/createuser.js
@@ -11,7 +11,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 
-router.use(cores({origin: 'https://food-api-theta.vercel.app', // Replace with your client's URL
+router.use(cores({origin: 'https://mern-food-mu.vercel.app', // Replace with your client's URL
 credentials: true, }));
 passport.use(new localpass(usermodel.authenticate()))
 
@@ -53,7 +53,7 @@ router.post("/login", passport.authenticate('local'), (req, res) => {
 });
 
 router.post('/foodData', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://food-api-theta.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://mern-food-mu.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
