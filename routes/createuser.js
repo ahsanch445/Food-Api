45a6d7ece483b1211679328eba006002e9ad4f56
Ahsan Ch <ach374392@gmail.com>
2023-12-18 11:15:33 -0800

    first commit

--- routes/createuser.js
+++ routes/createuser.js
@@ -11,7 +11,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 
-router.use(cores({ origin: '*' }));
+router.use(cores({  origin: 'http://localhost:3000',}));
 passport.use(new localpass(usermodel.authenticate()))
 
 
